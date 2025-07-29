a869d2d548c8546eac7fbc5a7f956ad1efdf88f4
BrentHoskins84 <brenthoskins84@gmail.com>
2025-07-29 09:46:14 -0700

    Remove node_modules, .env, and dist from tracking

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import "./functions/getUserReferrals";
-import "./functions/getUserReferralInfo";
-import "./functions/validateReferralCode";
-import "./functions/createReferral";
-import "./functions/documentation";
-import "./functions/health";
-export { mockDataService } from "./services/mockDataService";
-//# sourceMappingURL=index.d.ts.map

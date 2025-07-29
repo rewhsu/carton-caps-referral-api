5cd75cced06539d4e4897a74e2e607cc8791a9ea
BrentHoskins84 <brenthoskins84@gmail.com>
2025-07-29 12:12:58 -0700

    adding Google Build Config

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import "./functions/getUserReferrals";
 import "./functions/getUserReferralInfo";
 import "./functions/validateReferralCode";
 import "./functions/createReferral";
-import "./functions/documentation";
+// import "./functions/documentation";
 
 export { mockDataService } from "./services/mockDataService";
